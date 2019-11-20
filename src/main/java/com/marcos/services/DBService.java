7f8bedf34b8bb5f941ed56987d79414f525f22e1
Marcos <marcosapmf22@gmail.com>
2019-11-20 20:32:27 -0300

    populate test database

---
+++ src/main/java/com/marcos/services/DBService.java
@@ -0,0 +1,26 @@
+package com.marcos.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcos.model.Employee;
+import com.marcos.respositories.EmployeeRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	public void populateTestDataBase() {
+		
+		Employee e1 = new Employee("12345678910", "Bruce");
+		Employee e2 = new Employee("12345678911", "Clarck");
+		Employee e3 = new Employee("12345678912", "Diana");
+		
+		employeeRepository.saveAll(Arrays.asList(e1, e2, e3));
+	}
+	
+}
