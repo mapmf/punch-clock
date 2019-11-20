d628d094f7b3f3706569a4b3bb98ee1dfda87407
Marcos <marcosapmf22@gmail.com>
2019-11-20 20:27:52 -0300

    enable h2-database for test profile

---
+++ src/main/java/com/marcos/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.marcos.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**"
+	};
+	
+	@Autowired
+	Environment environment;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		if(Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		http.cors().and().csrf().disable();
+		
+		http.authorizeRequests()
+			.antMatchers(PUBLIC_MATCHERS).permitAll();
+	
+	}
+	
+}
