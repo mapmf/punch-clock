72008f11deb040e59ddc9259a79d31be178e72c2
Marcos <marcosapmf22@gmail.com>
2019-11-20 20:17:03 -0300

    create test profile

---
+++ src/main/java/com/marcos/config/TestConfig.java
@@ -0,0 +1,17 @@
+package com.marcos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Bean
+	public boolean instantiateDatabase() {
+		
+		return true;
+	}
+	
+}
