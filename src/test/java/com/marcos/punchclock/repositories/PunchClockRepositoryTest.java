80f0ce47444ce85d911379bb3ed08157688695ce
Marcos <marcosapmf22@gmail.com>
2019-11-20 22:53:09 -0300

    organize import

--- src/test/java/com/marcos/punchclock/repositories/PunchClockRepositoryTest.java
+++ src/test/java/com/marcos/punchclock/repositories/PunchClockRepositoryTest.java
@@ -7,7 +7,6 @@ import java.util.Date;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Bean;
 
 import com.marcos.punchclock.model.Employee;
 import com.marcos.punchclock.model.PunchClock;
