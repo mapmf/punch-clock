340d8010e95843fb7d93eb3a3e3fbc3a03450a5a
Marcos <marcosapmf22@gmail.com>
2019-11-20 21:00:28 -0300

    populate punchclock in test database

--- src/main/java/com/marcos/punchclock/services/DBService.java
+++ src/main/java/com/marcos/punchclock/services/DBService.java
@@ -1,18 +1,24 @@
 package com.marcos.punchclock.services;
 
 import java.util.Arrays;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcos.punchclock.model.Employee;
+import com.marcos.punchclock.model.PunchClock;
 import com.marcos.punchclock.respositories.EmployeeRepository;
+import com.marcos.punchclock.respositories.PunchClockRepository;
 
 @Service
 public class DBService {
 
 	@Autowired
 	EmployeeRepository employeeRepository;
+	
+	@Autowired
+	PunchClockRepository punchClockRepository;
 	
 	public void populateTestDataBase() {
 		
@@ -21,6 +27,20 @@ public class DBService {
 		Employee e3 = new Employee("12345678912", "Diana");
 		
 		employeeRepository.saveAll(Arrays.asList(e1, e2, e3));
+
+		Date date1 = new Date(System.currentTimeMillis() - 300000);
+		Date date2 = new Date(System.currentTimeMillis() - 400000);
+		Date date3 = new Date(System.currentTimeMillis() - 500000);
+		
+		PunchClock pc1 = new PunchClock(1, date1, e1);
+		PunchClock pc2 = new PunchClock(2, date2, e1);
+		PunchClock pc3 = new PunchClock(3, date3, e2);
+		
+		e1.getPunchClocks().addAll(Arrays.asList(pc1, pc2));
+		e2.getPunchClocks().addAll(Arrays.asList(pc3));
+		
+		punchClockRepository.saveAll(Arrays.asList(pc1, pc2, pc3));
+		
 	}
 	
 }
